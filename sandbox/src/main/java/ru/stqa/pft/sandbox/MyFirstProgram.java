802fe88813aaaadee00eaf64d89fd0809fd91812
your-morales <guns112811@gmail.com>
2022-04-01 14:00:36 +0300

    Закончил урок 15

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,25 +1,21 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-    public static void main(String[] args) {
-       hello("world");
-       hello("user");
-       hello("Andrey");
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("Andrey");
 
-       double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
-    }
-    public static  void hello (String somebody) {
-        System.out.println("Hello, " + somebody + "!");
-    }
-    public static double area(double len) {
-        return len * len;
-    }
-    public static  double area(double a, double b) {
-        return a * b;
-    }
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+
 }
