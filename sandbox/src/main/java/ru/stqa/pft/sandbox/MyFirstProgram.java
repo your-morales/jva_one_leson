82ce0256a89075082a1142d8ca3f86fc4a8e8d0b
your-morales <guns112811@gmail.com>
2022-04-04 11:08:43 +0300

    Первое задание

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -14,13 +14,9 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-    Point p1 = new Point();
-    p1.x1 = 1;
-    p1.y1 = 2;
+    Point p1 = new Point(4,6);
+    Point p2 = new Point(5,8);
 
-    Point p2 = new Point();
-    p2.x2 = 3;
-    p2.y2 = 4;
 
     System.out.println("Расстояние между двумя точками + " + distance(p1,p2));
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -2,10 +2,11 @@ package ru.stqa.pft.sandbox;
 
 public class Point {
 
-  double x1;
-  double y1;
-  double x2;
-  double y2;
+  double x;
+  double y;
 
-
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
 }
